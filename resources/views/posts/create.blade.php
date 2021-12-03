b9ef6c083cb1221b48963d43aef37f6ccdac3880
Ju-hyeon <mycom12317@gmail.com>
2021-12-03 14:14:52 +0900

    login

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -32,7 +32,7 @@
         </div>
 
         <div class="col-12 m-2">
-          <label for="applicant" class="form-label">모집인원</label>
+          <label for="applicant" class="form-label">모집인원(숫자만 입력해 주세요)</label>
           <textarea class="form-control" name="applicant" id="applicant">{{ old('applicant') }}</textarea>
           
           @error('applicant')
